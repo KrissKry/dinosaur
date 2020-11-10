627dc923911bdc19f7cf738c245360153005fdad
KrissKry <45544873+KrissKry@users.noreply.github.com>
2020-11-10 20:46:56 +0100

    obj detection, fps counter

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,147 @@
 #include <iostream>
+#include <chrono>
 #include "opencv2/opencv.hpp"
-// using namespace std;
+#include "opencv2/core.hpp"
+#define COLOR_MODE 0
+
+enum COLORS {ORANGE, GREEN, BLUE};
+
+int H_MIN = 0;
+int H_MAX = 256;
+int S_MIN = 0;
+int S_MAX = 256;
+int V_MIN = 0;
+int V_MAX = 256;
+
+struct gpreset {
+    int H_MIN = 42;
+    int H_MAX = 123;
+    int S_MIN = 42;
+    int S_MAX = 185;
+    int V_MIN = 93;
+    int V_MAX = 205;
+} gpreset;
+
+struct orangepreset {
+    int H_MIN = 3;
+    int H_MAX = 194;
+    int S_MIN = 98;
+    int S_MAX = 218;
+    int V_MIN = 200;
+    int V_MAX = 256;
+} orangepreset;
+
+int MIN_BALL_AREA = 10 * 10;
+
+std::string TRACKBAR_WINDOW = "Suwaki";
+
+void setMode() {
+
+    if (COLOR_MODE == ORANGE) {
+        H_MIN = orangepreset.H_MIN;
+        H_MAX = orangepreset.H_MAX;
+        S_MIN = orangepreset.S_MIN;
+        S_MAX = orangepreset.S_MAX;
+        V_MIN = orangepreset.V_MIN;
+        V_MAX = orangepreset.V_MAX;
+    } 
+    else if ( COLOR_MODE == GREEN) {
+        H_MIN = gpreset.H_MIN;
+        H_MAX = gpreset.H_MAX;
+        S_MIN = gpreset.S_MIN;
+        S_MAX = gpreset.S_MAX;
+        V_MIN = gpreset.V_MIN;
+        V_MAX = gpreset.V_MAX;
+    }
+}
+
+
+void trackbarValChange( int, void* ) {}
+
+void initTrackbars() {
+
+    cv::namedWindow(TRACKBAR_WINDOW, 0);
+    	
+    char TrackbarName[50];
+	sprintf( TrackbarName, "H_MIN", H_MIN);
+	sprintf( TrackbarName, "H_MAX", H_MAX);
+	sprintf( TrackbarName, "S_MIN", S_MIN);
+	sprintf( TrackbarName, "S_MAX", S_MAX);
+	sprintf( TrackbarName, "V_MIN", V_MIN);
+	sprintf( TrackbarName, "V_MAX", V_MAX);
+
+    cv::createTrackbar( "H_MIN", TRACKBAR_WINDOW, &H_MIN, H_MAX, trackbarValChange );
+    cv::createTrackbar( "H_MAX", TRACKBAR_WINDOW, &H_MAX, H_MAX, trackbarValChange );
+    cv::createTrackbar( "S_MIN", TRACKBAR_WINDOW, &S_MIN, S_MAX, trackbarValChange );
+    cv::createTrackbar( "S_MAX", TRACKBAR_WINDOW, &S_MAX, S_MAX, trackbarValChange );
+    cv::createTrackbar( "V_MIN", TRACKBAR_WINDOW, &V_MIN, V_MAX, trackbarValChange );
+    cv::createTrackbar( "V_MAX", TRACKBAR_WINDOW, &V_MAX, V_MAX, trackbarValChange );
+
+}
+
+
 
 int main() {
+    
+    cv::Mat frame;
+    cv::Mat frameHSV;
+    cv::Mat frameThreshold;
+    cv::VideoCapture webcam (0);
 
-    cv::VideoCapture webcam(0);
+    setMode();
+    //initTrackbars();
 
     if ( !webcam.isOpened() ) {
-        std::cerr << "Camera pepsi" << std::endl;
-        return 1; 
+        std::cout << "camera isded" << std::endl;
+        return 1;
     }
 
+    std::vector< std::vector< cv::Point >> contours;
+    std::vector< cv::Vec4i > hierarchy;
 
-    cv::namedWindow("window", cv::WINDOW_AUTOSIZE);
+    int licznik = 0;
+    auto start = std::chrono::steady_clock::now();
 
-    cv::Mat frame;
+    while (true) {
 
-    webcam >> frame;
+        webcam.read(frame);
+        licznik++;
 
-    while(1) {
-        cv::imshow("sexkamerka", frame);
+        cv::cvtColor(frame, frameHSV, cv::COLOR_BGR2HSV);
+    
+        cv::inRange(frameHSV, cv::Scalar(H_MIN, S_MIN, V_MIN), cv::Scalar(H_MAX, S_MAX, V_MAX), frameThreshold);
 
-        if (cv::waitKey(10) >= 0)
+        cv::findContours(frameThreshold, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
+        // std::sort(contours.begin(), contours.end(), cv::contourArea());
+
+
+        for( int i = 0; i < contours.size(); i++ ) 
+        {
+            if ( cv::contourArea(contours.at(i) ) > MIN_BALL_AREA )
+            {
+
+                cv::drawMarker(frame, contours.at(i).at(0), cv::Scalar(0,0,255));
+                break;
+            }
+        }        
+
+
+        cv::imshow("Kamerka", frame);
+        cv::imshow("Filtr", frameThreshold);
+
+        if ( cv::waitKey(1) >= 0 )
             break;
+    
     }
+
+    auto end = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> seconds = end - start;
+    
+
+    double klatki = licznik / seconds.count();
+    std::cout << klatki << " FPS" << std::endl;
+
+
     return 0;
 }
