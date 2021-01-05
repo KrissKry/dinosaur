20b835ba5471e964bad23320cb508bd899436bdb
KrissKry <edmaister02@gmail.com>
2021-01-05 13:43:00 +0100

    moved implementation to header file, minor changes

--- include/sharedqueue.hpp
+++ include/sharedqueue.hpp
@@ -8,15 +8,17 @@
 
 //https://www.softprayog.in/programming/interprocess-communication-using-posix-message-queues-in-linux?PageSpeed=noscript
 
-typedef struct coords_message {
-    int x{}, y{};
+struct coords_message {
+    int x{}, y{}; //coords of the object found
     std::chrono::system_clock::time_point timestamp{};
 };
 
-// typedef struct game_message {
-    // int y{}; //required y
-    // std::chrono::system_clock::time_point deadline{};
-// }
+struct game_message {
+    
+    int y{}; //required y
+    //subject to future changes .-.
+    std::chrono::system_clock::time_point deadline{};
+};
 
 class SharedQueue {
 
@@ -27,19 +29,37 @@ class SharedQueue {
 
     public:
 
-        SharedQueue(bool will_write);
-        ~SharedQueue();
+        SharedQueue(bool will_write, const char* queue_name) {
+            if (will_write)
+                // mq = mq_open(QUEUE_NAME, O_WRONLY | O_NONBLOCK);
+                mq = mq_open(queue_name, O_WRONLY);
+            else
+                // mq = mq_open(QUEUE_NAME, O_RDONLY | O_NONBLOCK);
+                mq = mq_open(queue_name, O_RDONLY);
+
+
+            mq_attr msg_attributes{};
+
+            mq_getattr(mq, &msg_attributes);
+            msg_size = msg_attributes.mq_msgsize;
+        }
+
+        ~SharedQueue() {
+            mq_close(mq);
+        }
 
         template <typename T>
-        void push(T* msg);
+        void push(T* msg_struct) {
+
+            mq_send(mq, (const char *) msg_struct, sizeof(T), 0);
+        }
 
         template <typename T>
-        void pop(T* msg);
+        void pop(T* msg_struct) {
 
+            int result = mq_receive(mq, (char *) msg_struct, sizeof(T), NULL);
+        }
 
-
-        // void push(message msg); //ablo strukture msg (chyba lepiej tak będzie)
-        // message pop();
 };
 
-#endif;
+#endif
